2e7710a7924f19856af79fe0c53453f9c2d86056
Yiannis Galanakis <igalas@hotmail.co.uk>
2024-04-28 14:02:33 +0100

    newlines

--- tests/test_get_actions.py
+++ tests/test_get_actions.py
@@ -9,4 +9,3 @@ def test_get_all_actions():
         assert res.status_code == 200
         expected = [{"codeword": 5001, "id": "alert"}, {"codeword": 5002, "id": "thanks"}]
         assert expected == json.loads(res.get_data(as_text=True))
-
